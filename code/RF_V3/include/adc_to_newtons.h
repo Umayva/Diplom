b383124e44fbc05ac532f8995164bf8094665cd1
Umayva <m1shams@mail.ru>
2023-10-31 21:58:38 +0300

    3

--- code/RF_V3/include/adc_to_newtons.h
+++ code/RF_V3/include/adc_to_newtons.h
@@ -4,11 +4,12 @@
  * Prototype version. Author: Aymeric Wang
  */
 
-#ifndef ADC_TO_NEWTS_H
-#define ADC_TO_NEWTS_H
+#ifndef FORCE_SENSOR_H
+#define FORCE_SENSOR_H
 
 #include <Arduino.h>
 #include <math.h>
+
 
 #define FORCE_SENSOR_PIN 36
 #define RESISTANCE 5000.00
@@ -18,25 +19,25 @@
 #define COEFF_FORCE_SENSOR_LAW 93.199
 #define EXP_FORCE_SENSOR_LAW 1.277
 
-/** * * * * * * * * * * * * * * * * * *
- * @brief Sample code for setup() and loop()
- * 
- * @setup 
- * void setup() {
- *   Serial.begin(115200);
- * }
- * 
- * @loop
- * void loop(){
- *  uint16_t analogReading = analogRead(FORCE_SENSOR_PIN);
- *  float resistance = bridgeDivider(RESISTANCE,analogReading);
- *  Serial.print("ADC:\t");
- *  Serial.print(analogReading);
- *  Serial.print("\t\tResistance:\t");
- *  Serial.println(resistance);
- *  delay(500);
- * }
- * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * *
+* Recommended settings of void setup()
+* 
+* @setup 
+* void setup() {
+*   Serial.begin(115200);
+* }
+* 
+* @loop
+* void loop(){
+*     uint16_t analogReading = analogRead(FORCE_SENSOR_PIN);
+*     float resistance = bridgeDivider(RESISTANCE,analogReading);
+*     Serial.print("ADC:\t");
+*     Serial.print(analogReading);
+*     Serial.print("\t\tResistance:\t");
+*     Serial.println(resistance);
+*     delay(500);
+* }
+* * * * * * * * * * * * * * * * * * * */
 
 
 float voltageDivider(float resistance, uint16_t ADC_measurement){
---
+++ code/RF_V3/include/sender.h
@@ -0,0 +1,51 @@
+/*
+ * Remote Feelings: Adam Curtis, Aymeric Wang, Xinying Hu
+ * 11/30/21
+ * Prototype version. Author: Aymeric Wang
+ */
+
+#ifndef SENDER_H
+#define SENDER_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include "calibration.h"
+
+void test_UART() { 
+  Serial.println("MASTURBATOR3000 приказать");
+  delay(1500);
+}
+
+void message_send(String message){
+  String formatted_message;
+  formatted_message ="<"+message+">";
+  Serial.print(formatted_message);
+}
+
+void send_control() {
+  static int i = 0;
+  int output;
+
+  for (i = 0; i < (numOfFingers - 1); i++){
+    output = map(fingerPos[i], fingerPosMin[i], fingerPosMax[i], 0, 255);
+    if(output > 255) output = 255;
+    if(output < 0) output = 0;
+    Serial.print(output);
+    Serial.println("");
+
+    // output = map(fingerPos[i], fingerPosMin[i], fingerPosMax[i], 0, 255);
+    // if(output > 255) output = 255;
+    // if(output < 0) output = 0;
+    // Serial.print(output);
+    // Serial.print(",");
+  }
+  
+  // output = map(fingerPos[i], fingerPosMin[i], fingerPosMax[i], 0, 255);
+  // if(output > 255) output = 255;
+  // if(output < 0) output = 0;
+  // Serial.print(output);
+  // Serial.println("");
+
+}
+
+#endif
